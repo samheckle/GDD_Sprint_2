a60e5062d72e9d45728d85ff494e72da60f6d033
Sam Heckle <samheckle@yahoo.com>
2017-10-05 16:38:15 -0400

    added gyroscope prototype

---
+++ ArmaballoTM/Assets/ActualGyroscopeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class ActualGyroscopeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+		transform.rotation = new Quaternion(0,0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 transform.rotation = GyroToUnity(Input.gyro.attitude);
+		 Debug.Log(Input.gyro.attitude);
+	}
+
+	 protected void OnGUI()
+    {
+        GUI.skin.label.fontSize = Screen.width / 40;
+
+        GUILayout.Label("Orientation: " + Screen.orientation);
+        GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude);
+        GUILayout.Label("iphone width/font: " + Screen.width + " : " + GUI.skin.label.fontSize);
+    }
+
+	private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, -q.z, q.y, q.w);
+    }
+}
