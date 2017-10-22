bd9997a8108116699dc935aab35a21e78084aa07
Nikolas Whiteside <nrw6218@rit.edu>
2017-10-22 18:49:37 -0400

    Finished functionality for Title Screen and Level Select Screen.

--- ArmaballoTM/Assets/Scripts/AdvanceToScene.cs
+++ ArmaballoTM/Assets/Scripts/AdvanceToScene.cs
@@ -17,7 +17,18 @@ public class AdvanceToScene : MonoBehaviour {
 		
 	}
 
+	/// <summary>
+	/// Loads the scene stored in the sceneName field
+	/// </summary>
 	public void LoadScene(){
 		SceneManager.LoadScene (sceneName);
+	}
+
+	/// <summary>
+	/// Loads a scene with a given name
+	/// </summary>
+	/// <param name="name">Name.</param>
+	public void LoadScene(string name){
+		SceneManager.LoadScene (name);
 	}
 }
---
+++ ArmaballoTM/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public List<GameObject> buttons;
+	private int playerProgress;
+
+	// Use this for initialization
+	void Start () {
+		InitializeButtons ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// Sets up level-select buttons dependent o
+	/// player progress.
+	/// </summary>
+	void InitializeButtons(){
+		//Set progress to current stored in PlayerPrefs, otherwise set it to zero
+		playerProgress = PlayerPrefs.GetInt ("Progress", 0);
+		//Loop through and activate the buttons available
+		for (int i = 0; i < playerProgress + 1; i++) {
+			buttons [i].GetComponent<Button> ().interactable = true;
+		}
+	}
+}
