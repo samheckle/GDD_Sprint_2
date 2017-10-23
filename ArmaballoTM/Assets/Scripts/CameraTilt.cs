93e4225aa83c982949cbefdf330d81ce409cd25e
Sam Heckle <samheckle@yahoo.com>
2017-10-22 21:03:21 -0400

    attempting to create first official level

---
+++ ArmaballoTM/Assets/Scripts/CameraTilt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTilt : MonoBehaviour {
+
+	public GameObject gameObj;
+	public GameObject sphere;
+	public ActualGyroscopeScript gyroScript;
+	// Use this for initialization
+	void Start () {
+		gyroScript = gameObj.GetComponent<ActualGyroscopeScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion rotY = transform.rotation;
+		rotY.y = gyroScript.rotation.y*-.001f;
+		rotY.z = gyroScript.rotation.z*-.001f;
+		Vector3 moveX = transform.position;
+		// if(moveX.x == sphere.transform.position.x){
+		 	//moveX += sphere.transform.position;
+		// }
+		// else{
+		// moveX.x += sphere.transform.position.x*.001f;
+		// }
+		// rotY.y = gyroScript.rotation.y*.001f;
+		// Debug.Log(rotY);
+		transform.rotation = rotY;
+		transform.position = moveX;
+	}
+}
